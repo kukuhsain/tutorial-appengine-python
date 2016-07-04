c9a635b4f0b961b6d8c0b78f4f1ecd016b633dd9
Kukuh <kukuhsain@gmail.com>
2016-07-04 14:40:44 +0700

    Add hashing functionality to cookie in project 3 (using cookie)

--- 3-using-cookie/play.py
+++ 3-using-cookie/play.py
@@ -1,11 +1,23 @@
 import os
 import webapp2
 import jinja2
+import hashlib
 
 from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
+
+def hash_str(s):
+	return hashlib.md5(s).hexdigest()
+
+def make_secure_value(s):
+	return "%s|%s" % (s, hash_str(s))
+
+def check_secure_value(s):
+	value = s.split('|')[0]
+	if s == make_secure_value(value):
+		return value
 
 class Handler(webapp2.RequestHandler):
 	"""Handler Prototype"""
@@ -24,13 +36,17 @@ class MainPage(Handler):
 	def get(self):
 		visits = self.request.cookies.get('visits', '0')
 
+		visits = check_secure_value(visits)
+
 		if visits.isdigit():
 			visits = int(visits)
 			visits += 1
 		else:
 			visits = 0
 
-		self.response.headers.add_header('Set-Cookie', 'visits=%s' % visits)
+		visits_cookie = make_secure_value(str(visits))
+
+		self.response.headers.add_header('Set-Cookie', 'visits=%s' % visits_cookie)
 
 		self.render("index.html", visits=visits)
 
