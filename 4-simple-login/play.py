15ba04269120a11dd73ef002b1bea0e9e97a45e4
Kukuh <kukuhsain@gmail.com>
2016-07-05 05:19:26 +0700

    Add hashing password functionality using hmac in project simple login

--- 4-simple-login/play.py
+++ 4-simple-login/play.py
@@ -2,19 +2,29 @@ import os
 import webapp2
 import jinja2
 import hashlib
+import hmac
 
 from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
+# 'user_id': 123,
+# 'username': 'kukuh',
+# 'password': 'hunterpass',
+# 'secret': 'secretpass',
+
+SECRET = 'secretpass'
+
 CREDENTIAL = {
+				'user_id': 123,
 				'username': 'kukuh',
-				'password': 'hunterpass'
+				'hmac_password': 'f8e72b971aced103b676af2f95affb08',
 			}
 
 def hash_str(s):
-	return hashlib.md5(s).hexdigest()
+	# return hashlib.md5(s).hexdigest()
+	return hmac.new(SECRET, s).hexdigest()
 
 def make_secure_value(s):
 	return "%s|%s" % (s, hash_str(s))
@@ -45,9 +55,11 @@ class MainPage(Handler):
 
 	def post(self):
 		check_username = CREDENTIAL['username'] == self.request.get('username')
-		check_password = CREDENTIAL['password'] == self.request.get('password')
+		hashed_password = hash_str(self.request.get('password'))
+		check_password = CREDENTIAL['hmac_password'] == hashed_password
 
 		if check_username and check_password:
+			print make_secure_value(self.request.get('password'))
 			self.render("dashboard.html")
 		else:
 			self.render("login.html")
