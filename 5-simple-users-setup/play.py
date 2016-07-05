90744eb3c21a1728ac853155dd488096740e2f08
Kukuh <kukuhsain@gmail.com>
2016-07-05 16:17:39 +0700

    Add a project of a simple setup users(register, login, logout)

--- 5-simple-users-setup/play.py
+++ 5-simple-users-setup/play.py
@@ -4,7 +4,7 @@ import jinja2
 import hashlib
 import hmac
 
-from google.appengine.ext import db
+from google.appengine.ext import ndb
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
@@ -22,19 +22,28 @@ CREDENTIAL = {
 				'hmac_password': 'f8e72b971aced103b676af2f95affb08',
 			}
 
-def hash_str(s):
-	# return hashlib.md5(s).hexdigest()
-	return hmac.new(SECRET, s).hexdigest()
+class Hashing():
+	def hash_str(s):
+		# return hashlib.md5(s).hexdigest()
+		return hmac.new(SECRET, s).hexdigest()
 
-def make_secure_value(s):
-	return "%s|%s" % (s, hash_str(s))
+	def make_secure_value(s):
+		return "%s|%s" % (s, hash_str(s))
 
-def check_secure_value(s):
-	value = s.split('|')[0]
-	if s == make_secure_value(value):
-		return value
-	else:
-		return '0'
+	def check_secure_value(s):
+		value = s.split('|')[0]
+		if s == make_secure_value(value):
+			return value
+		else:
+			return '0'
+
+class User(ndb.Model):
+	userid = ndb.IntegerProperty(required=True)
+	username = ndb.StringProperty(required=True)
+	password = ndb.StringProperty(required=True)
+	email = ndb.StringProperty()
+	created_date = ndb.DateTimeProperty(auto_now_add=True)
+
 
 class Handler(webapp2.RequestHandler):
 	"""Handler Prototype"""
@@ -49,9 +58,9 @@ class Handler(webapp2.RequestHandler):
 		self.write(self.render_str(template, **kw))
 		
 
-class MainPage(Handler):
+class HomePage(Handler):
 	def get(self):
-		self.render("login.html")
+		self.render("home.html")
 
 	def post(self):
 		check_username = CREDENTIAL['username'] == self.request.get('username')
@@ -63,6 +72,35 @@ class MainPage(Handler):
 		else:
 			self.render("login.html")
 
+class LoginPage(Handler):
+	def get(self):
+		self.render("login.html")
+
+	def post(self):
+		pass
+
+class LogoutPage(Handler):
+	def get(self):
+		self.redirect('/')
+
+	def post(self):
+		self.redirect('/')
+
+class RegisterPage(Handler):
+	def get(self):
+		self.render("register.html")
+
+	def post(self):
+		pass
+
+class DashboardPage(Handler):
+	def get(self):
+		self.render("dashboard.html")
+
 app = webapp2.WSGIApplication([
-	('/', MainPage),
+	('/', HomePage),
+	('/login', LoginPage),
+	('/logout', LogoutPage),
+	('/register', RegisterPage),
+	('/dashboard', DashboardPage),
 	], debug=True)
